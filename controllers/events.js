4028c54bcabd373978a8b47ff30a486d4c928cf9
Williams Mata <williams.rm99@gmail.com>
2023-02-14 17:33:10 -0400

    eliminarEvento controller finished

--- controllers/events.js
+++ controllers/events.js
@@ -39,7 +39,7 @@ const actualizarEvento = async (req = request, res = response) => {
     const evento = await Evento.findById(eventoId);
 
     if (!evento) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: "Evento no existe por ese id",
       });
@@ -76,11 +76,40 @@ const actualizarEvento = async (req = request, res = response) => {
   }
 };
 
-const eliminarEvento = (req = request, res = response) => {
-  res.json({
-    ok: true,
-    msg: "Eliminar evento",
-  });
+const eliminarEvento = async (req = request, res = response) => {
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const evento = await Evento.findById(eventoId);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Evento no existe por ese id",
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio para eliminar este evento",
+      });
+    }
+
+    await Evento.findByIdAndDelete(eventoId);
+
+    res.status(200).json({
+      ok: true,
+      msg: "Evento eliminado",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 module.exports = {
