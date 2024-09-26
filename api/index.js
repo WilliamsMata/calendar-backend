e437811612daa3b4a57792e929a7841f08f13d4b
williamsmata <williams.rm99@gmail.com>
2024-09-26 12:20:03 -0400

    Add api folder to handle vercel request

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { dbConnection } = require("./database/config");
+const { dbConnection } = require("../database/config");
 require("dotenv").config();
 
 // Crear el servidor de express
@@ -17,8 +17,8 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // Rutas
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/events", require("./routes/events.routes"));
+app.use("/api/auth", require("../routes/auth.routes"));
+app.use("/api/events", require("../routes/events.routes"));
 
 app.get("/*", (req, res) => {
   res.redirect("/");
@@ -31,3 +31,5 @@ dbConnection().then(() => {
     console.log(`Servidor corriendo en puerto ${port}`);
   });
 });
+
+module.exports = app;
